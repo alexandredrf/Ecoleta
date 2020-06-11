900324b2bb72d281d1fbeea500437b4f2301fb17
Alexandre Davi <alexandre_drf@hotmail.com>
2020-06-11 16:08:33 -0300

    4º Aula final

--- README.md
+++ README.md
@@ -3,3 +3,6 @@ Treinamento dev week
 
 
 no console npm install express
+
+Com o auxíliod do nunkucks é possível utiizar os arquivos html de forma dinâmica -> utilizando variáveis , objetos dividindo o html
+ex: pasta partials --> São arquivos que são utilizados com frequência em um ou mais arquivos html da pasta views
--- src/server.js
+++ src/server.js
@@ -4,26 +4,42 @@ const server = express()
 //Configurar pasta píblica
 server.use(express.static("public"))
 
+//Utilizando TEMPLATE ENGINE  Transforma html acessível para funções.
+
+const nunjucks = require("nunjucks") // 1:00:00 do vídeo  // pedindo um módulo , uma dependência
+nunjucks.configure("src/views",{  //1º Local onde ta os htmls
+
+    express: server,   // ligação do nunjucks com o server
+    noCache: true
+})
+
+
 // Configurar camonhos para a minha aplicação
 //Página incial
 //req: Requisição (Pergunta)
 //res: Resposta
+
+
+// ROTAS
 server.get("/",function(req, res) {  // Server pede uma barra , envia a barra e executa uma função
 
-    res.sendFile(__dirname +"/views/index.html")
+   return res.render("index.html",{title:"Um Título"})   //variáveis utilizando render
 })
 
 
 server.get("/create-point",function(req, res) {  // Server pede uma barra , envia a barra e executa uma função
 
-    res.sendFile(__dirname +"/views/create-point.html")
+    return res.render("create-point.html")
 })
 
 
 server.get("/search-results",function(req, res) {  // Server pede uma barra , envia a barra e executa uma função
 
-    res.sendFile(__dirname +"/views/search-results.html")
+   return res.render("search-results.html")     //trocar sendfiles por render
 })
+//FIM DE ROTAS
+
+
 
 // ligar o servidor
 server.listen(3000) //fica ouvindo na porta 3000 
